432489c886131c92855c428326d28abcfbbdc475
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2024-09-03 21:14:07 +0200

    nix

--- apps/mobile-e2e/playwright.config.ts
+++ apps/mobile-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run moss:serve',
+    command: 'npx nx run mobile:serve',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
